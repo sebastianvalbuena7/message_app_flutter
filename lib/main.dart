552c69da3abc909a40d7ffde05e00b1bbce7638e
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-08-08 21:48:12 -0500

    Theme en Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:message_app/config/theme/app_theme.dart';
+import 'package:message_app/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,16 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 3).theme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: FilledButton.tonal(onPressed: () {}, child: const Text('Clic me')),
-        ),
-      ),
+      home: const ChatScreen(),
     );
   }
 }
-
-// https://github.com/sebastianvalbuena7/message_app_flutter.git
---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:message_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My love♥️'),
+        centerTitle: false, // center title
+        leading: const Padding(
+          padding: EdgeInsets.all(3.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://static.photocdn.pt/images/articles/2018/12/05/articles/2017_8/beginner_photography_mistakes-1.webp'),
+          ),
+        ),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              // itemCount: 10,
+              itemBuilder: (context, index) {
+                return const MyMessageBubble();
+              },
+            )),
+            const Text('Mundo')
+          ],
+        ),
+      ),
+    );
+  }
+}
